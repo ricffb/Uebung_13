a0ac8f4e1804899f22e989442968cf7373f942e2
ricffb <ricffb@gmail.com>
2018-02-05 05:25:31 +0100

    small improvement

--- src/Map.java
+++ src/Map.java
@@ -1,13 +1,28 @@
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.stream.Stream;
 
 public class Map {
 
-    public static <X,Y> LinkedList<Y> map(Function<X,Y> f, LinkedList<X> list) {
+   /*
+   public static <X,Y> LinkedList<Y> map(Function<X,Y> f, LinkedList<X> list) {
 
         LinkedList<Y> res = new LinkedList<>();
         for (X x : list){
             res.add(f.apply(x));
         }
+        return res;
+
+    }
+    */
+
+    public static <X,Y> LinkedList<Y> map(Function<X,Y> f, LinkedList<X> list) {
+
+        LinkedList<Y> res = new LinkedList<>();
+
+
+        list.forEach((X x) -> res.add(f.apply(x)));
+
         return res;
 
     }
